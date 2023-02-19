8ade3542bad9ec48a629db9c43d9c023c370eef6
teppei <teppei0830.4869@gmail.com>
2023-02-19 22:18:10 +0900

    snippet_list.html とそれに対するルーティングを追加しました。
    これは表示のテスト段階です。
    今後、指定された言語の指定された分類に含まれるスニペットのみを
    表示できるようにしたいと考えています。

--- snippet_app/urls.py
+++ snippet_app/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'snippet_app'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('list/', views.SnippetListView.as_view(), name='list'),
 ]
 
--- snippet_app/views.py
+++ snippet_app/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Lang, Type, Snippet
 # Create your views here.
 
 class IndexView(TemplateView):
 
     def get(self, request):
         return render(request, 'snippet_app/index.html')
+
+class SnippetListView(ListView):
+    model = Snippet
+    def get_context_data(self, **kwargs):
+        my_dict = {
+            'lang_list': Lang.objects.all(),
+            'type_list': Type.objects.all(),
+        }
+        return super().get_context_data(**my_dict)
+
