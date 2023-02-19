86a4a510f4f2cdfb16ced188aa56457cb2501d0a
teppei <teppei0830.4869@gmail.com>
2023-02-19 15:46:17 +0900

    models.py にモデルを登録しました。
    Lnag, Type, Snippet という基本構成を登録しています。
    Lang は言語。
    Type は言語の中での分類。
    Snippet は分類の中での実際のスニペットです。

--- snippet_app/admin.py
+++ snippet_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Lang, Type, Snippet
 # Register your models here.
+
+admin.site.register(Lang)
+admin.site.register(Type)
+admin.site.register(Snippet)
--- snippet_app/models.py
+++ snippet_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Lang(models.Model):
+    lang = models.CharField(max_length=100)
+class Type(models.Model):
+    type = models.CharField(max_length=100)
+
+    ### 他のテーブルとの関連
+    lang = models.ForeignKey(Lang, on_delete=models.PROTECT)
+class Snippet(models.Model):
+    title = models.CharField(max_length=100)
+    code = models.TextField()
+
+    ### 他のテーブルとの関連
+    type = models.ForeignKey(Type, on_delete=models.PROTECT)
