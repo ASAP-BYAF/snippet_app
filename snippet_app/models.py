56f434e05d0dfb364823be2226f2bae0157387c5
teppei <teppei0830.4869@gmail.com>
2023-02-19 22:16:38 +0900

    model に __str__ 属性を追加しました。
    これは、管理サイトでの一覧表示の際に、 ID と 名前を表示させると
    どのようなデータが登録されているかを確認しやすいと考えたからです。

--- snippet_app/models.py
+++ snippet_app/models.py
@@ -3,14 +3,23 @@ from django.db import models
 # Create your models here.
 class Lang(models.Model):
     lang = models.CharField(max_length=100)
+    def __str__(self):
+        return f'<ID = {self.id}> {self.lang}'
+
 class Type(models.Model):
     type = models.CharField(max_length=100)
 
     ### 他のテーブルとの関連
     lang = models.ForeignKey(Lang, on_delete=models.PROTECT)
+    def __str__(self):
+        return f'<ID = {self.id}> {self.type}'
+
 class Snippet(models.Model):
     title = models.CharField(max_length=100)
     code = models.TextField()
 
     ### 他のテーブルとの関連
     type = models.ForeignKey(Type, on_delete=models.PROTECT)
+    def __str__(self):
+        return f'<ID = {self.id}> {self.title}'
+
