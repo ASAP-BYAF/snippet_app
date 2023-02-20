ace348ad84a33c93eb36db80810262491a2f5a0d
teppei <teppei0830.4869@gmail.com>
2023-02-20 22:33:35 +0900

    snippet_list.html において言語に一致したスニペットだけを表示するようにしました。
    snippet_list.html の各言語に <a> タグを埋め込み、各言語に遷移するようにしました。
    これに伴って、 get_context_data の type_list は一致する言語に属する分類のみを取得するようにしました。
    また、クエリセットの type のデフォルト値も言語に応じて設定されるようにしました。

--- snippet_app/views.py
+++ snippet_app/views.py
@@ -11,14 +11,16 @@ class IndexView(TemplateView):
 class SnippetListView(ListView):
     model = Snippet
     def get_context_data(self, **kwargs):
+        lang = Lang.objects.get(id=self.request.GET.get('lang', 1))
         my_dict = {
             'lang_list': Lang.objects.all(),
-            'type_list': Type.objects.all(),
+            'type_list': lang.type_set.all(),
         }
         return super().get_context_data(**my_dict)
     def get_queryset(self):
         lang = Lang.objects.get(id=self.request.GET.get('lang', 1))
-        type = lang.type_set.get(id=self.request.GET.get('type', 1))
+        type = lang.type_set.get(id=self.request.GET.get('type',\
+            lang.type_set.first().id))
         self.queryset = type.snippet_set.all()
         return super().get_queryset()
 
