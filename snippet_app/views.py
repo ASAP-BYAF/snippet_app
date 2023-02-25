1dacba82d076bf58fa1e5b4cc61962925c7e5142
teppei <teppei0830.4869@gmail.com>
2023-02-25 14:08:40 +0900

    views.py を修正しました。
    User に登録されているモデルに関連するスニペットのみを表示するための
    基本形を作成しました。
    現在は、 User が public に固定になっています。
    今後は、これらをログインしているユーザーに応じて変更できるようにしたいと考えています。

--- snippet_app/views.py
+++ snippet_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
-from .models import Lang, Type, Snippet
+from .models import Lang, Type, Snippet, User
 # Create your views here.
 
 class IndexView(TemplateView):
@@ -11,15 +11,19 @@ class IndexView(TemplateView):
 class SnippetListView(ListView):
     model = Snippet
     def get_context_data(self, **kwargs):
-        lang = Lang.objects.get(id=self.request.GET.get('lang', 1))
+        user = User.objects.get(id=1)
+        lang = Lang.objects.get(id=self.request.GET.get('lang', \
+            user.lang_set.first().id))
         my_dict = {
-            'lang_list': Lang.objects.all(),
+            'lang_list': user.lang_set.all(),
             'lang': lang,
             'type_list': lang.type_set.all(),
         }
         return super().get_context_data(**my_dict)
     def get_queryset(self):
-        lang = Lang.objects.get(id=self.request.GET.get('lang', 1))
+        user = User.objects.get(id=1)
+        lang = Lang.objects.get(id=self.request.GET.get('lang', \
+            user.lang_set.first().id))
         type = lang.type_set.get(id=self.request.GET.get('type',\
             lang.type_set.first().id))
         self.queryset = type.snippet_set.all()
