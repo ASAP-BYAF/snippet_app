57777200a0be57d27bf97d643903f910abe523d2
teppei <teppei0830.4869@gmail.com>
2023-02-26 13:37:37 +0900

    views.py において lang, type, snippet のそれぞれが登録されていないときの 例外処理を追加しました。
    これは、Web 上でページ遷移をしてみて、
    うまく動作していることを確認しました。

--- snippet_app/views.py
+++ snippet_app/views.py
@@ -18,21 +18,36 @@ class SnippetListView(ListView):
     def get_context_data(self, **kwargs):
         user = self.get_user()
         lang = self.get_lang()
+        if lang is not None:
+            type_list = lang.type_set.all()
+        else:
+            type_list = None
         my_dict = {
             'user_list': CustomUser.objects.all(),
             'lang_list': user.lang_set.all(),
             'lang': lang,
-            'type_list': lang.type_set.all(),
+            'type_list': type_list,
         }
         return super().get_context_data(**my_dict)
 
     def get_queryset(self):
-        lang = self.get_lang()
         type = self.get_type()
-        self.queryset = type.snippet_set.all()
+        if type is not None:
+            self.queryset = type.snippet_set.all()
+        else:
+            self.queryset = None
+
         return super().get_queryset()
 
+    def get_template_names(self):
+        # スニペットが一つも作成されていないとき template を変更
+        # 現在テスト中であり、代わりのテンプレートとして、 index.html
+        #　を使っている。今後、適切な内容に変更が必要。
+        if self.queryset is None:
+            self.template_name = 'snippet_app/index.html'
+        return super().get_template_names()
     def get_user(self):
+        # 指定されてユーザーを取得。デフォルトは開発者。
         return CustomUser.objects.get(id=self.request.GET.get('user', 1))
 
     def get_lang(self):
@@ -40,10 +55,15 @@ class SnippetListView(ListView):
         try:
             return Lang.objects.get(id=self.request.GET.get('lang', \
                                                             user.lang_set.first().id))
+        # lang が一つも登録されていないとき
         except AttributeError:
             return None
 
     def get_type(self):
         lang = self.get_lang()
-        return lang.type_set.get(id=self.request.GET.get('type', \
-                                                         lang.type_set.first().id))
+        try:
+            return lang.type_set.get(id=self.request.GET.get('type', \
+                                                             lang.type_set.first().id))
+        # type が一つも登録されていないとき
+        except AttributeError:
+            return None
