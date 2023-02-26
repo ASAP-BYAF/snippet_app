c648365f94eef0fa250237288e2c625487ee3e8d
teppei <teppei0830.4869@gmail.com>
2023-02-26 11:42:21 +0900

    snippet_list.html と views.py を変更しました。
    CustomUser に紐づいた snippet だけを表示するためのテストのためです。
    現在は、 id = 1 の user にしか対応していません。
    今後、 ほかのユーザーにも対応できるように修正が必要です。

--- snippet_app/views.py
+++ snippet_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
-from .models import Lang, Type, Snippet, User
+from .models import Lang, Type, Snippet
+from accounts.models import CustomUser
 # Create your views here.
 
 class IndexView(TemplateView):
@@ -11,17 +12,18 @@ class IndexView(TemplateView):
 class SnippetListView(ListView):
     model = Snippet
     def get_context_data(self, **kwargs):
-        user = User.objects.get(id=1)
+        user = CustomUser.objects.get(id=self.request.GET.get('user', 1))
         lang = Lang.objects.get(id=self.request.GET.get('lang', \
             user.lang_set.first().id))
         my_dict = {
+            'user_list': CustomUser.objects.all(),
             'lang_list': user.lang_set.all(),
             'lang': lang,
             'type_list': lang.type_set.all(),
         }
         return super().get_context_data(**my_dict)
     def get_queryset(self):
-        user = User.objects.get(id=1)
+        user = CustomUser.objects.get(id=self.request.GET.get('user', 1))
         lang = Lang.objects.get(id=self.request.GET.get('lang', \
             user.lang_set.first().id))
         type = lang.type_set.get(id=self.request.GET.get('type',\
