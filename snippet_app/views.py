89ed100917d41011bdfeca16b22289d5872d7f3d
teppei <teppei0830.4869@gmail.com>
2023-02-26 12:17:27 +0900

    views.py において SnippetListView の user, lang を得るメソッドを追加しました。
    user については、前回実装したメソッドを修正しました。
    user を class のインスタンス変数として定義するのをやめ、
    代わりにメソッドの返り値として設定しました。
    理由は、 __init__ メソッド以外の場所でインスタンス変数を
    定義するのが pythonic ではない(可読性が低い)からです。

--- snippet_app/views.py
+++ snippet_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
 from .models import Lang, Type, Snippet
 from accounts.models import CustomUser
+
+
 # Create your views here.
 
 class IndexView(TemplateView):
@@ -9,15 +11,13 @@ class IndexView(TemplateView):
     def get(self, request):
         return render(request, 'snippet_app/index.html')
 
+
 class SnippetListView(ListView):
     model = Snippet
-    def get(self, request):
-        self.get_user()
-        return super().get(self.request)
+
     def get_context_data(self, **kwargs):
-        user = self.user
-        lang = Lang.objects.get(id=self.request.GET.get('lang', \
-            user.lang_set.first().id))
+        user = self.get_user()
+        lang = self.get_lang()
         my_dict = {
             'user_list': CustomUser.objects.all(),
             'lang_list': user.lang_set.all(),
@@ -25,15 +25,18 @@ class SnippetListView(ListView):
             'type_list': lang.type_set.all(),
         }
         return super().get_context_data(**my_dict)
+
     def get_queryset(self):
-        user = self.user
-        lang = Lang.objects.get(id=self.request.GET.get('lang', \
-            user.lang_set.first().id))
-        type = lang.type_set.get(id=self.request.GET.get('type',\
-            lang.type_set.first().id))
+        lang = self.get_lang()
+        type = lang.type_set.get(id=self.request.GET.get('type', \
+                                                         lang.type_set.first().id))
         self.queryset = type.snippet_set.all()
         return super().get_queryset()
 
     def get_user(self):
-        self.user = CustomUser.objects.get(id=self.request.GET.get('user', 1))
-        return
+        return CustomUser.objects.get(id=self.request.GET.get('user', 1))
+
+    def get_lang(self):
+        user = self.get_user()
+        return Lang.objects.get(id=self.request.GET.get('lang', \
+                                                        user.lang_set.first().id))
