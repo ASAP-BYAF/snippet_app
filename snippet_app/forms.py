01304e1f91bcf3fe22d7477d6bdb27b4420eec50
teppei <teppei0830.4869@gmail.com>
2023-03-05 15:54:48 +0900

    forms.py においてフィールドにラベルを付けました。

--- snippet_app/forms.py
+++ snippet_app/forms.py
@@ -7,19 +7,25 @@ class SnippetForm(forms.ModelForm):
     class Meta:
         model = Snippet
         fields = ['title', 'code', 'explanation',]
-
+        labels={
+            'title': 'タイトル',
+            'code': 'コード',
+            'explanation': '説明'
+            }
     def __init__(self, person, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         lang_queryset = Lang.objects.filter(user__id=person)
         lang_list = [(i_lang.id, i_lang.lang) for i_lang in lang_queryset]
-        type_list = [(i_type.id, i_type.type) for i_lang in lang_queryset
+        type_list = [(f'{i_lang.id}.{i_type.id}', i_type.type) for i_lang in lang_queryset
             for i_type in i_lang.type_set.all()]
         self.fields['lang'] = forms.ChoiceField(
             choices=lang_list,
-            widget = forms.RadioSelect
+            widget = forms.RadioSelect,
+            label = '言語'
         )
         self.fields['type'] = forms.ChoiceField(
             choices = type_list,
-            widget = forms.RadioSelect
+            widget = forms.RadioSelect,
+            label = '分類'
         )
