365be0a69f9c7331388a92f7c0093583b19d611c
teppei <teppei0830.4869@gmail.com>
2023-03-06 22:01:17 +0900

    snippet_form.html において選択されている言語に属する分類だけが表示されるようにしました。

--- static/snippet_app/js/main.js
+++ static/snippet_app/js/main.js
@@ -8,11 +8,24 @@ createForm.addEventListener('change', handleChange);
 console.log(langList);
 
 function handleChange() {
-        for (let i=0; i<langList.length; i++) {
-            const i_opt = langList[i];
+        for (let i_lang=0; i_lang<langList.length; i_lang++) {
+            const i_opt = langList[i_lang];
             if (i_opt.value == 0) {
                 displayField(i_opt.checked, newLangField);
                 displayField(i_opt.checked, newTypeField);
+            }
+            if (i_opt.checked) {
+                for (let j_type=0; j_type<typeList.length; j_type++) {
+                    const j_opt = typeList[j_type];
+                    console.log(j_opt.value);
+                    console.log(j_opt.value.split('.')[0]);
+                    if (i_opt.value == j_opt.value.split('.')[0]){
+                        displayField(true, j_opt.parentElement);
+                    } else {
+                        displayField(false, j_opt.parentElement);
+                        j_opt.checked = false;
+                    }
+                }
             }
         }
 }
