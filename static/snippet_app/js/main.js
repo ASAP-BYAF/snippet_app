255dd35d19b9a48f7cfb9c214d5a10810320b8ed
teppei <teppei0830.4869@gmail.com>
2023-03-12 16:35:30 +0900

    main.js を変更しました。
    選択肢の新しい分類が選ばれたときにのみの新しい分類のための記入欄が
    現れるようにしました。

--- static/snippet_app/js/main.js
+++ static/snippet_app/js/main.js
@@ -5,29 +5,28 @@ const newLangField = document.getElementById('id_lang_new');
 const newTypeField = document.getElementById('id_type_new');
 createForm.addEventListener('change', handleChange);
 
-console.log(langList);
-
 function handleChange() {
-        for (let i_lang=0; i_lang<langList.length; i_lang++) {
-            const i_opt = langList[i_lang];
-            if (i_opt.value == 0) {
-                displayField(i_opt.checked, newLangField);
-                displayField(i_opt.checked, newTypeField);
-            }
-            if (i_opt.checked) {
-                for (let j_type=0; j_type<typeList.length-1; j_type++) {
-                    const j_opt = typeList[j_type];
-                    console.log(j_opt.value);
-                    console.log(j_opt.value.split('.')[0]);
-                    if (i_opt.value == j_opt.value.split('.')[0]){
-                        displayField(true, j_opt.parentElement);
-                    } else {
-                        displayField(false, j_opt.parentElement);
-                        j_opt.checked = false;
-                    }
+    for (let i_lang=0; i_lang<langList.length; i_lang++) {
+        const i_opt = langList[i_lang];
+        if (i_opt.checked) {
+            for (let j_type=0; j_type<typeList.length-1; j_type++) {
+                const j_opt = typeList[j_type];
+                if (i_opt.value == j_opt.value.split('.')[0]){
+                    displayField(true, j_opt.parentElement);
+                } else {
+                    displayField(false, j_opt.parentElement);
+                    j_opt.checked = false;
                 }
             }
         }
+        if (i_opt.value == 0) {
+            displayField(i_opt.checked, newLangField);
+            if (i_opt.checked) {
+                typeList[typeList.length-1].checked = true;
+            }
+        }
+        displayField(typeList[typeList.length-1].checked, newTypeField)
+    }
 }
 
 function displayField(ifDisplay, target){
