ab29d0b37a41baa792aec1c2050e2f43d47f134f
teppei <teppei0830.4869@gmail.com>
2023-05-17 21:22:36 +0900

    api/urls.py を修正しました。
    FromSnippetView に対する url を snippet/ から from_snippet/ に変更して、関係をわかりやすくしました。

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'api_app'
 urlpatterns = [
     path('user_snippet', views.CustomUserSnippetList.as_view(), name='user_snippet'),
-    path('snippet', views.FromSnippetList.as_view(), name='from_snippet'),
+    path('from_snippet', views.FromSnippetList.as_view(), name='from_snippet'),
     path('lang', views.LangList.as_view(), name='lang'),
     path('user/<int:pk>', views.user_detail),
 ]
