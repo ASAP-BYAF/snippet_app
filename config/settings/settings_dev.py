c5cd3230552b2794327dbda79e90b64f77210e74
teppei <teppei0830.4869@gmail.com>
2023-05-17 21:10:23 +0900

    settings_dev.py を修正しました。
    API の開発時も Debug = True で実行できそうなので、これらの設定を元に戻しました。
    api の view を class 型に変えたことでこれが可能になったような気がします

--- config/settings/settings_dev.py
+++ config/settings/settings_dev.py
@@ -1,8 +1,7 @@
 from .settings_common import *
 
-# DEBUG = True
-DEBUG = False
-ALLOWED_HOSTS = ['*']
+DEBUG = True
+ALLOWED_HOSTS = []
 
 # 開発時はアプリから送信されるメールの内容を terminal に出力。
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
