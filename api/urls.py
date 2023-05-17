56705f2b5eff3eac120354ea62e6885395eb4a76
teppei <teppei0830.4869@gmail.com>
2023-05-17 21:12:19 +0900

    api/urls.py を修正しました。
    CustomUser を取得するという view に応じて、適切名前を付けました。

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     # path('signup', views.user_list),
-    path('signup', views.CustomUserList.as_view()),
+    path('user', views.CustomUserList.as_view()),
     path('snippet', views.FromSnippetList.as_view()),
     path('lang', views.LangList.as_view()),
     path('user/<int:pk>', views.user_detail),
