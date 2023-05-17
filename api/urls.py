5c4c8b9004069ff79dbfc29adb70e494cb7cc803
teppei <teppei0830.4869@gmail.com>
2023-05-17 21:16:45 +0900

    api/urls.py を修正しました。
    アプリ名と書く url に対して name 属性をつけることで逆引きをしやすくしました。
    また、 user/ の url について紐づいた snippet を取得することがわかるように
    uesr_snippet/ に変更しました。

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'api_app'
 urlpatterns = [
-    # path('signup', views.user_list),
-    path('user', views.CustomUserList.as_view()),
-    path('snippet', views.FromSnippetList.as_view()),
-    path('lang', views.LangList.as_view()),
+    path('user_snippet', views.CustomUserList.as_view(), name='user_snippet'),
+    path('snippet', views.FromSnippetList.as_view(), name='from_snippet'),
+    path('lang', views.LangList.as_view(), name='lang'),
     path('user/<int:pk>', views.user_detail),
 ]
